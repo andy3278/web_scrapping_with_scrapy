603b81c882315cbaddcd484bd1e8fe7c4f02de24
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-12-13 07:26:59 -0500

    Update pipelines.py

--- stack/stack/pipelines.py
+++ stack/stack/pipelines.py
@@ -6,7 +6,7 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import settings
 
 class StackPipeline:
     def process_item(self, item, spider):
